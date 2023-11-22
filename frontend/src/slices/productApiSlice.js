69dd4eef4b954e5041bb0df4ce99454c9a39fac8
Divyansh1406 <divyanshvashist01@gmail.com>
2023-11-22 13:58:44 +0530

    c2

--- frontend/src/slices/productApiSlice.js
+++ frontend/src/slices/productApiSlice.js
@@ -34,7 +34,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
     }),
     uploadProductImage: builder.mutation({
       query: (data) => ({
-        url: `/api/upload`,
+        url: `${UPLOADS_URL}`,
         method: "POST",
         body: data,
       }),
