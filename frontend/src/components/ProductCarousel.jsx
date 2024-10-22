dd787315fb2a1eaf7ffd84127e506955d1440d86
Divyansh1406 <divyanshvashist01@gmail.com>
2024-10-22 20:34:41 +0530

    Update ProductCarousel.jsx

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -6,14 +6,40 @@ import "./Product.css";
 
 const ProductCarousel = () => {
   const { data: products, isLoading, error } = useGetTopProductsQuery();
+  const productz = [
+    {
+      _id: "1",
+      image:
+        "https://res.cloudinary.com/dvn5ulypk/image/upload/v1729609377/furryfriendngo/uliaknqaidcueemcj1vw.jpg",
+      linkto: "https://strayanimalfoundationindia.org/",
+      name: "site 1",
+      headline: "A BETTER LIFE FOR INDIA'S STRAY ANIMALS",
+    },
+    {
+      _id: "2",
+      image:
+        "https://res.cloudinary.com/dvn5ulypk/image/upload/v1729609378/furryfriendngo/gvsdng1h69n7thcvux1u.jpg",
+      linkto: "https://amtmindia.org/",
+      name: "site 2",
+      headline: "Help Us Help Them",
+    },
+    {
+      _id: "3",
+      image:
+        "https://res.cloudinary.com/dvn5ulypk/image/upload/v1729609378/furryfriendngo/xvfh1hcqdvnosclvvlqn.jpg",
+      linkto: "https://www.peopleforanimalsindia.org/",
+      name: "site 3",
+      headline: "People For Animals",
+    },
+  ];
 
   return isLoading ? null : error ? (
     <Message variant="danger">{error?.data?.message || error.error}</Message>
   ) : (
     <Carousel pause="hover" className="bg-light mb-4">
-      {products.map((product) => (
+      {productz.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
+          <Link to={`{product.linkto}`}>
             <Image
               src={product.image}
               alt={product.name}
@@ -21,9 +47,7 @@ const ProductCarousel = () => {
               style={{ height: "60vh" }}
             />
             <Carousel.Caption className="carousel-caption">
-              <h2 className="text-white text-left">
-                {product.name} (₹{product.price})
-              </h2>
+              <h2 className="text-white text-left">{product.headline}</h2>
             </Carousel.Caption>
           </Link>
         </Carousel.Item>
